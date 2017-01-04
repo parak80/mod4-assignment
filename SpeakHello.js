73e939f0262337b83a26375bc7bd11a0b1a63522
parisa Carlborg <parak80@gmail.com>
2017-01-04 16:56:36 +0100

    initializing

---
+++ SpeakHello.js
@@ -0,0 +1,9 @@
+﻿(function (window) {
+    var helloSpeaker = {};
+
+    var speakWord = "Hello";
+    helloSpeaker.speak = function (name) {
+        console.log(speakWord + " " + name);
+    }
+    window.helloSpeaker = helloSpeaker;
+})(window);
